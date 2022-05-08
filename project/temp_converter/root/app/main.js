b91c2437929a8690505ad44251409bdd98449b1b
sattarranjbar1370 <iran.kalkoush@gmail.com>
2022-05-08 15:05:01 +0430

    Select DOM elements

---
+++ project/temp_converter/root/app/main.js
@@ -0,0 +1,8 @@
+// 
+
+// select item in DOM
+let spanChangeLabel = document.getElementById('change-label');
+let inputValue = document.querySelector('input');
+let btnReset = document.getElementById('reset');
+let btnConvert = document.getElementById('convert');
+let btnChange = document.getElementById('change');
