447439b4227b533e96f63355357351ca92f7ac14
sattarranjbar1370 <iran.kalkoush@gmail.com>
2022-05-19 19:33:09 +0430

    last edition

--- project/temp_converter/root/app/main.js
+++ project/temp_converter/root/app/main.js
@@ -1,8 +1,17 @@
+
 // 
+// variable
+let $ = document;
+
 
 // select item in DOM
-let spanChangeLabel = document.getElementById('change-label');
-let inputValue = document.querySelector('input');
-let btnReset = document.getElementById('reset');
-let btnConvert = document.getElementById('convert');
-let btnChange = document.getElementById('change');
+let spanChangeLabel = $.getElementById('change-label');
+let inputValue = $.querySelector('input');
+let btnReset = $.getElementById('reset');
+let btnConvert = $.getElementById('convert');
+let btnChange = $.getElementById('change');
+
+
+btnChange.addEventListener('click', function changeAlgo() {
+    console.log(spanChangeLabel.value)
+})
